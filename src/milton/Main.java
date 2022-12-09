5ed5d5d897e04460764ae8f88397bae9716d9f9d
Daryl_Moh <Daryl.Moh@hotmail.com>
2022-12-09 16:10:05 +0800

    Second commit for Milton example

--- src/milton/Main.java
+++ src/milton/Main.java
@@ -1,9 +1,16 @@
 package milton;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
+
+    public static final String HEADER = "word, count";
 
     public static void main(String[] args) throws Exception {
         String fileName = args[0];
@@ -16,18 +23,58 @@ public class Main {
         String line;
         FileReader fr = new FileReader(fileName);
         BufferedReader br = new BufferedReader(fr);
+        
+        Integer totalWords = 0;
+        Map<String, Integer> wordMap = new HashMap<>();
 
-        Integer i = 1;
-        while (i <= 100) {
+        for (Integer i = 1; i <= 100; i++) {
             line = br.readLine();
             if (null == line)
                 break;
-            System.out.printf("%d: %s\n", i, line.toUpperCase());
-            i++;
-        }
+            //System.out.printf("%d: %s\n", i, line.toUpperCase());
+            String[] words = line.trim().split(" ");
+            totalWords += words.length;
+            for (String w: words) {
+                String cleanWord = w.replaceAll(",","");
+                Integer v = wordMap.getOrDefault(cleanWord, 0);
+                v++;
+                wordMap.put(cleanWord, v);
+                // if (wordMap.containsKey(w)) {
+                //     Integer v = wordMap.get(w) + 1;
+                //     wordMap.put(w, v);
+                // } else
+                //     wordMap.put(w, 1);
+            }
+
+        } // End For
 
         br.close();
         fr.close();
+
+        Set<String> uniqueWords = wordMap.keySet();
+
+        // for (String w: uniqueWords)
+        //     System.out.printf("> %s: %d\n", w, wordMap.get(w));
+
+        System.out.printf("The number of words in first 100 lines: %d\n"
+                , totalWords);
+        System.out.printf("Number of unque words: %d\n", uniqueWords.size());
+
+        FileOutputStream fos = new FileOutputStream(args[1]);
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        
+        osw.write(HEADER);
+
+        for (String w: wordMap.keySet()) {
+
+            String line2 = String.format("%s,%d\n",w, wordMap.get(w));
+            osw.write(line2);
+        }
+        
+        osw.flush();
+        osw.close();
+        fos.close();
+
     }
 
 }
